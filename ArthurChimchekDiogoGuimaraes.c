64f2e95ecde7025657db2d906f36838b5e01885e
dioguima <diogoferreiraguima@hotmail.com>
2017-05-23 20:23:37 -0300

    Fourth commit

--- ArthurChimchekDiogoGuimaraes.c
+++ ArthurChimchekDiogoGuimaraes.c
@@ -8,59 +8,121 @@ typedef struct nodeQueue {
 	struct nodeQueue *next;
 }node;
 
-#define FILE_PATH "c://teste.txt"
+typedef struct estrutura_paginas_virtuais {
+	int valor;
+	struct estrutura_paginas_virtuais *prox;
+} stPagVirtual;
 
-bool has(node **firstNode, int value);
-void pushBack(node ** lastNode, int newValue);
+stPagVirtual *inicio, *pagina, *pag_ant;
+
 int popFront(node ** firstNode);
 int getItem(node **);
 int getQueueSize(node **firstNode);
 
+bool has(node **firstNode, int value);
+void pushBack(node ** lastNode, int newValue);
+void printQueue(node **firstNode);
+
 void main() {
+	int valor, maxQueueSize, pageFaults, i;
+	char req[10];
+	node *currentNode, *firstNode;
+	FILE *arquivo;
 
-	int maxQueueSize = 4;
-	int pageFaults = 0;
+	valor = NULL;
+	maxQueueSize = 64;
+	pageFaults = 0;
 
-	int pages[] = { 3, 2, 1, 3, 5, 4, 6, 3, 7, 4 };
+	arquivo = fopen("tests/file4.txt", "r");
+	pagina = malloc(sizeof(struct estrutura_paginas_virtuais));
+	inicio = pagina;
+	pag_ant = pagina;
+	inicio->valor = valor;
+	fgets(req, 9, arquivo);       			/* Le o arquivo */
+	valor = atoi(req);						/* Converte o string lido para inteiro */
+	//printf("\nValor lido: %d", valor);
+	inicio->valor = valor;
+	inicio->prox = NULL;
 
-	int arrSize = (int)(sizeof(pages) / sizeof(pages[0]));
-	int i;
-	node *firstNode;
-	node *currentNode = (node *)malloc(sizeof(node));;
-	currentNode->value = pages[0];
+	while (!feof(arquivo))
+	{
+		fgets(req, 9, arquivo);				/* Le o arquivo */
+		if (!feof(arquivo))					/* Verifica se é fim de arquivo */
+		{
+			valor = atoi(req);
+			//printf("\nValor lido: %d", valor);
+
+			pagina = malloc(sizeof(stPagVirtual)); 		/* Aloca nova página na memória  */
+			pagina->valor = valor;
+
+			pag_ant->prox = pagina;						/* Inclui a página na estrutura */
+			pag_ant = pagina;
+			pagina->prox = NULL;
+		}
+	}
+	pagina = inicio;
+
+	
+
+	currentNode = (node *)malloc(sizeof(node));;
+	currentNode->value = pagina->valor;
 	currentNode->next = NULL;
-	firstNode = currentNode;
-	for (i = 1; i < arrSize; i++) {
-		if (has(&firstNode, pages[i])) {
-			printf("Tem na memoria\n");
+	firstNode = (node*)currentNode;
+
+	while (pagina != NULL)			/* passa por toda a lista e mostra o conteúdo  */
+	{
+		if (has(&firstNode, pagina->valor)) {
+			//printf("Tem na memoria\n");
 		}
 		else {
 			if (getQueueSize(&firstNode) == maxQueueSize) {
+				//printf("\n\n%d\n\n", getQueueSize(&firstNode));
 				popFront(&firstNode);
 				pageFaults++;
 			}
-			pushBack(&currentNode, pages[i]);
+			pushBack(&currentNode, pagina->valor);
 		}
+		//printf("\nValor pedido: %d", pagina->valor);
+		//printQueue(&firstNode);
+		pagina = pagina->prox;
 	}
 
 	//printf("%d\n", getQueueSize(&firstNode));
-	printf("%d\n", popFront(&firstNode));
-	printf("%d\n", popFront(&firstNode));
-	printf("%d\n", popFront(&firstNode));
-	printf("%d\n", popFront(&firstNode));
-	printf("%d\n", getQueueSize(&firstNode));
+	//printf("%d\n", popFront(&firstNode));
+	//printf("%d\n", popFront(&firstNode));
+	//printf("%d\n", popFront(&firstNode));
+	//printf("%d\n", popFront(&firstNode));
+	//printf("%d\n", getQueueSize(&firstNode));
 	//printf("%d\n", has(&firstNode, 9));
 	//printf("%d\n", has(&firstNode, 4));
 
-	printf("Arquivo %d, %5d páginas, %7d requisições: FIFO: %5d Page Faults", 1, maxQueueSize, arrSize, pageFaults);
+	printf("Arquivo %d, %d páginas, %d requisições: FIFO: %d Page Faults", 1, maxQueueSize, 9999, pageFaults);
 
 	scanf("");
 }
 
+void printQueue(node **firstNode) {
+	node *temp;
+	if (firstNode == NULL) {
+		printf("\n\n --- Lista vazia !!!");
+	}
+	else {
+		printf("\n\n --- ");
+		node * firstNodeB = (*firstNode);
+		temp = (node *)malloc(sizeof(node));
+		temp = firstNodeB;
+		while (temp != NULL) {
+			printf("%d ", temp->value);
+			temp = temp->next;
+		}
+		printf("\n");
+	}
+}
+
 bool has(node **firstNode, int value) {
 	node *temp;
-	if (firstNode == NULL || (*firstNode)->value == NULL) {
-		printf("Lista vazia !!!");
+	if (firstNode == NULL) {
+		//printf("Lista vazia !!!");
 	}
 	else {
 		node * firstNodeB = (*firstNode);
@@ -77,7 +139,7 @@ bool has(node **firstNode, int value) {
 
 void pushBack(node ** lastNode, int newValue) {
 	node *newLastNode;
-	if (lastNode == NULL || (*lastNode)->value == NULL) {
+	if (lastNode == NULL) {
 		newLastNode = (node *)malloc(sizeof(node));
 		newLastNode = (*lastNode);
 		newLastNode->value = newValue;
